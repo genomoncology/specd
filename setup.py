a5b81bd41516f38ef735f2fca27dfaed02b3b284
ewennerb <ewennerb@purdue.edu>
2018-07-09 17:01:04 -0400

    Version bump to 0.2.1, fixes bravado troubles

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('dev-requirements.txt') as dev_requirements_file:
 
 setup(
     name="specd",
-    version='0.2.0',
+    version='0.2.1',
 
     package_dir={
         '': 'src'
@@ -37,9 +37,9 @@ setup(
         "dictdiffer >= 0.7.1",
         "related >= 0.6.3",
         "inflect >= 0.3.1",
-        "aiobravado == 0.9.2",
-        "bravado == 9.3.0",
-        "bravado-core == 5.0.3",
+        "aiobravado == 0.9.3",
+        "bravado == 10.0.0",
+        "bravado-core == 5.0.4",
         "bravado-asyncio == 1.0.0",
         "stringcase >= 1.2.0",
     ],
